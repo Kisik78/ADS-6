36c81c44630fb49fcd01851587515230ebaab479
Коньков Арсений <95272498+Kisik78@users.noreply.github.com>
2022-04-15 22:51:27 +0300

    Konkov

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,10 +1,27 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+  private:
+  T arr[100];
+  int fst;
+  int lst;
+ 
+  public:
+  TPQueue() : fst(0), lst(0) {}
+  void push(T uni) {
+    int lat = lst++;
+    while ((--lat >= fst) && (arr[lat % size].prior < uni.prior)) {
+      arr[(lat + 1) % size] = arr[lat % size];
+    }
+    arr[(lat + 1) % size] = uni;
+  }
+  T pop() {
+    return arr[(fst++) % size];
+  }
 };
 
 struct SYM {
